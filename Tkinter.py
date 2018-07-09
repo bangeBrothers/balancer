fd6adbed08549ff97818630b49b15aeea57cec88
galvarino <gandalfbangstein@gmail.com>
2018-07-09 19:06:01 -0400

    Test commit 2
    Signed-off-by: galvarino <gandalfbangstein@gmail.com>

--- Tkinter.py
+++ Tkinter.py
@@ -1,11 +1,15 @@
 from tkinter import *
 root = Tk()
-win = Canvas(root, width=500, height=500)
+win = Canvas(root, width=400, height=500)
 root.resizable(False, False)
 win.pack()
-win.mainloop()
 
 
 def Button_Function():
     print("fuckgot")
 
+
+Butt = Button(root, text="Make a fuckgot", command=Button_Function)
+Butt.pack()
+
+win.mainloop()
